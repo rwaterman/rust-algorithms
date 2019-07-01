d235cf9a596a9310a6f84f76dc1c2e8339b00bd4
Rick Waterman <rwaterman@users.noreply.github.com>
2019-07-01 12:43:21 -0700

    tests: added for gcd
    Signed-off-by: Rick Waterman <rwaterman@users.noreply.github.com>

--- src/math/discrete.rs
+++ src/math/discrete.rs
@@ -3,3 +3,24 @@ pub fn gcd(p: u64, q: u64) -> u64 {
   let r = p % q;
   return gcd(q, r);
 }
+
+
+#[cfg(test)]
+mod tests {
+  use crate::math::discrete::gcd;
+
+  #[test]
+  fn test_gcd_composite() {
+    assert_eq!(gcd(10, 15), 5);
+  }
+
+  #[test]
+  fn test_gcd_prime() {
+    assert_eq!(gcd(13, 17), 1);
+  }
+
+  #[test]
+  fn test_gcd_prime_and_composite() {
+    assert_eq!(gcd(5, 17), 1);
+  }
+}
